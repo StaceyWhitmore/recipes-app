e467987cd7e03b4a7f0ddc76c96a057e0c3eac8d
StaceyWhitmore <staceywhitmore@gmail.com>
2018-10-17 17:43:20 -0600

    installed loader dev dependencies for bundeling css (css/Menu.css) and added loaders to webpack.config.js module

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Recipe from './Recipe'
+import '../../css/Menu.css'
 
 const Menu = ({recipes}) =>
   <article>
--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,14 @@ module.exports = {
         query: {
           presets: ['env', 'stage-0', 'react']
         }
+      },
+      {
+        test:/\.css$/,
+        use: ['style-loader','css-loader', {
+            loader:'postcss-loader',
+            options: {
+              plugins: () => [require('autoprefixer')]
+            }}]
       }
     ]
   }
