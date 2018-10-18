c218ff6f0d76dd0bfd04e8f010cb9f99573b21f4
StaceyWhitmore <staceywhitmore@gmail.com>
2018-10-17 19:09:26 -0600

    add default props to stateless functional component

--- src/Components/stateless_functional_components/Summary.js
+++ src/Components/stateless_functional_components/Summary.js
@@ -1,11 +1,13 @@
-const Summary = ({ingredients, steps, title}) => {
+const Summary = ({ingredients=0, steps=0, title='[recipe]'}) => {
   return <div>
     <h1>{title}</h1>
     <p>{ingredients} Ingredients | {steps} Steps</p>
   </div>
 }
 
+/*
 Summary.propTypes = {
   ingredients: PropTypes.number,
   steps: PropTypes.number,
-} //close Summary.PropTypes
+}
+*/
