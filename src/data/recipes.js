8411143bdfbbdded9ce8575072bc08c249f1489f
StaceyWhitmore <staceywhitmore@gmail.com>
2018-10-17 21:23:03 -0600

    added empty spaces in data for better formatting

--- src/data/recipes.js
+++ src/data/recipes.js
@@ -2,12 +2,12 @@ const data = [
   {
     "name": "Oven Salmon",
     "ingredients": [
-      {"name": "Salmon","amount": 1, "measurement": "1 lb."},
-      {"name": "diezse Nuts crushed","amount": 1, "measurement": "cup"},
-      {"name": "Butter Balm","amount": 2, "measurement": "cups"},
-      {"name": "ChimmiChurri","amount": 3, "measurement": "tsp."},
-      {"name": "Avacado Oil","amount": .5, "measurement": "cup"},
-      {"name": "Challets","amount": 5, "measurement": "teeth"}
+      {"name": " Salmon","amount": 1, "measurement": " lb."},
+      {"name": " diezse Nuts crushed","amount": 1, "measurement": " cup"},
+      {"name": " Butter Balm","amount": 2, "measurement": " cups"},
+      {"name": " ChimmiChurri","amount": 3, "measurement": " tsp."},
+      {"name": " Avacado Oil","amount": .5, "measurement": " cup"},
+      {"name": " Challets","amount": 5, "measurement": " teeth"}
     ],
     "steps": [
       "step 1 is .....",
@@ -20,18 +20,18 @@ const data = [
   {
     "name": "Marinera Sauce",
     "ingredients": [
-      {"name": "Olive Oil","amount": 0.5, "measurement": "cup"},
-      {"name": "Onions","amount": 2, "measurement": "med"},
-      {"name": "Garlic","amount": 5, "measurement": "cloves"},
-      {"name": "Oregano","amount": 0.5, "measurement": "cup"},
-      {"name": "Fresh Basil","amount": 0.5, "measurement": "cup"},
-      {"name": "Caldo de Vaca","amount": .5, "measurement": "tsp."},
-      {"name": "Butter","amount": 1, "measurement": "Tbls"},
-      {"name": "Mushrooms","amount": 1, "measurement": "cup"},
-      {"name": "Black Olives","amount": .5, "measurement": "cup"},
-      {"name": "Italian Sausage","amount": 1, "measurement": "1 lb."},
-      {"name": "Sugar","amount": 0.5, "measurement": "tsp."},
-      {"name": "Malbec Wine","amount": 1, "measurement": "a gusto"}
+      {"name": " Olive Oil","amount": 0.5, "measurement": " cup"},
+      {"name": " Onions","amount": 2, "measurement": " med"},
+      {"name": " Garlic","amount": 5, "measurement": " cloves"},
+      {"name": " Oregano","amount": 0.5, "measurement": " cup"},
+      {"name": " Fresh Basil","amount": 0.5, "measurement": " cup"},
+      {"name": " Caldo de Vaca","amount": .5, "measurement": " tsp."},
+      {"name": " Butter","amount": 1, "measurement": " tbs"},
+      {"name": " Mushrooms","amount": 1, "measurement": " cup"},
+      {"name": " Black Olives","amount": .5, "measurement": " cup"},
+      {"name": " Italian Sausage","amount": 1, "measurement": " lb."},
+      {"name": " Sugar","amount": 0.5, "measurement": " tsp."},
+      {"name": " Malbec Wine","amount": 1, "measurement": " a gusto"}
     ],
     "steps": [
       "step 1 is .....",
